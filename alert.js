e279f24658953851ac1b57e63859b9852bf0b2d4
Kwekugrind <166255201+Kwekugrind@users.noreply.github.com>
2026-07-18 15:37:20 +0000

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,4 +1,6 @@
-const ws = new WebSocket("wss://ws.derivws.com/websockets/v3?app_id=1089");
+import WebSocket from "ws";
+
+const ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
 
 ws.on("open", () => {
   ws.send(JSON.stringify({ active_symbols: "brief", product_type: "basic" }));
